3d937f3a539f77b8d91130517a75df7689c05a6e
Ravi <Ravi@Ravi-PC>
2020-08-22 20:51:37 +0530

    First Commit

---
+++ SeleniumJenkins/src/test/java/org/ravikiran/selenium/SeleniumJenkins/FirstJenkins.java
@@ -0,0 +1,36 @@
+package org.ravikiran.selenium.SeleniumJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FirstJenkins {
+  
+  private WebDriver driver;
+	String appURL = "www.google.com";
+
+	@BeforeClass
+	public void testSetUp() {
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C://Users//Ravi//Marsworkspace//SeleniumJenkins//Drivers//chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void verifyGooglePageTittle() {
+		driver.get("https://www.google.com");
+		String getTitle = driver.getTitle();
+		System.out.println("The title of the page is"+getTitle);
+		System.out.println("Your script is passs");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
